01b10af6081058fc203c1c15350cda13444e092c
Tung Pham <tpham20908@gmail.com>
2020-02-01 10:08:06 -0500

    setup schema and resolvers

---
+++ src/entity/User.ts
@@ -0,0 +1,16 @@
+import { Entity, ObjectIdColumn, ObjectID, Column, BaseEntity } from "typeorm";
+
+@Entity("users")
+export class User extends BaseEntity {
+  @ObjectIdColumn()
+  id: ObjectID;
+
+  @Column("varchar", { length: 255 })
+  email: string;
+
+  @Column("text")
+  password: string;
+
+  @Column()
+  age: number;
+}
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import "reflect-metadata";
+import { importSchema } from "graphql-import";
+import { GraphQLServer } from "graphql-yoga";
+import { createConnection } from "typeorm";
+
+import { resolvers } from "./resolvers";
+const typeDefs = importSchema(`${__dirname}/schema.graphql`);
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+createConnection().then(() => {
+  server.start(() => console.log("Server is running on localhost:4000"));
+});
